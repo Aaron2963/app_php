9ca057759b90d39182cdde3dd70b646601c7251d
Aaron2963 <neotgrr@gmail.com>
2023-04-07 10:36:59 +0800

    新增權限範圍的判斷

--- src/authorization/OAuthAuthorization.php
+++ src/authorization/OAuthAuthorization.php
@@ -21,40 +21,25 @@ class OAuthAuthorization implements AuthorizationInterface
         $this->PublicKeyUri = $PublicKeyUri;
     }
     
-    public function Authorize($Token)
+    public function Authorize($Token, $ResourceScopes = [])
     {
         // get public key
         $PublicKey = file_get_contents($this->PublicKeyUri);
         try {
             $Decoded = JWT::decode($Token, new Key($PublicKey, 'RS256'));
             if ($this->IsTokenRevoked($Decoded->jti)) {
-                return false;
+                throw new \Exception('token is revoked');
             }
-            return true;
-        } catch (InvalidArgumentException $e) {
-            // provided key/key-array is empty or malformed.
-            $this->Error = $e;
-        } catch (DomainException $e) {
-            // provided algorithm is unsupported OR
-            // provided key is invalid OR
-            // unknown error thrown in openSSL or libsodium OR
-            // libsodium is required but not available.
-            $this->Error = $e;
-        } catch (SignatureInvalidException $e) {
-            // provided JWT signature verification failed.
-            $this->Error = $e;
-        } catch (BeforeValidException $e) {
-            // provided JWT is trying to be used before "nbf" claim OR
-            // provided JWT is trying to be used before "iat" claim.
-            $this->Error = $e;
-        } catch (ExpiredException $e) {
-            // provided JWT is trying to be used after "exp" claim.
-            $this->Error = $e;
-        } catch (UnexpectedValueException $e) {
-            // provided JWT is malformed OR
-            // provided JWT is missing an algorithm / using an unsupported algorithm OR
-            // provided JWT algorithm does not match provided key OR
-            // provided key ID in key/key-array is empty or invalid.
+            if (empty($ResourceScopes)) {
+                return true;
+            }
+            foreach ($ResourceScopes as $Scope) {
+                if (in_array($Scope, $Decoded->scopes)) {
+                    return true;
+                }
+            }
+            throw new \Exception('token does not have required scopes');
+        } catch (\Exception $e) {
             $this->Error = $e;
         }
         return false;
