a6a2ffdcd9ce228c6bdbfda00c9ff910a8ea2ee3
Aaron2963 <neotgrr@gmail.com>
2024-08-09 18:16:44 +0800

    rewrite OAuthAuthorization unit test to fix coverage test

--- tests/OAuthAuthorizationTest.php
+++ tests/OAuthAuthorizationTest.php
@@ -1,58 +1,111 @@
 <?php
 
-namespace Lin\AppPhp\Tests;
+namespace Lin\AppPhp\Tests\Authorization;
 
 use Lin\AppPhp\Authorization\OAuthAuthorization;
 use PHPUnit\Framework\TestCase;
 use Firebase\JWT\JWT;
 
+class SampleAuth extends OAuthAuthorization
+{
+    protected function IsTokenRevoked($JTI)
+    {
+        return $JTI !== 'abc123';
+    }
+}
+
+/**
+ * @covers \Lin\AppPhp\Authorization\OAuthAuthorization
+ */
 class OAuthAuthorizationTest extends TestCase
 {
+    private $publicKeyUri = __DIR__ . '/asset/public.key';
+
     /**
-     * @covers OAuthAuthorization::Authorize, OAuthAuthorization::IsTokenRevoked, OAuthAuthorization::__construct, OAuthAuthorization::GetError
+     * @covers \Lin\AppPhp\Authorization\OAuthAuthorization::Authorize
+     * @covers \Lin\AppPhp\Authorization\OAuthAuthorization::IsTokenRevoked
      */
-    public function testAuthorize()
+    public function testAuthorizeValidToken()
     {
-        //generate a JWT
-        $Key = file_get_contents(__DIR__ . '/asset/private.key');
-        $Payload = [
-            'iss' => 'http://example.com',
-            'sub' => '1234567890',
-            'aud' => 'http://example.org',
-            'exp' => time() + 3600,
-            'iat' => time(),
-            'jti' => 'PASS',
-            'scopes' => ['doc:read', 'doc:write']
+        $authorization = $this->getMockForAbstractClass(OAuthAuthorization::class, [$this->publicKeyUri]);
+        $authorization->expects($this->once())
+            ->method('IsTokenRevoked')
+            ->with('abc123')
+            ->willReturn(false);
+
+        $token = $this->generateValidToken(['scopes' => ['read', 'write']]);
+        $this->assertTrue($authorization->Authorize($token, ['read', 'write']));
+    }
+
+    /**
+     * @covers \Lin\AppPhp\Authorization\OAuthAuthorization::Authorize
+     * @covers \Lin\AppPhp\Authorization\OAuthAuthorization::IsTokenRevoked
+     * @covers \Lin\AppPhp\Authorization\OAuthAuthorization::GetError
+     */
+    public function testAuthorizeTokenWithMissingScopesThrowsException()
+    {
+        $authorization = $this->getMockForAbstractClass(OAuthAuthorization::class, [$this->publicKeyUri]);
+        $authorization->expects($this->once())
+            ->method('IsTokenRevoked')
+            ->with('abc123')
+            ->willReturn(false);
+
+        $token = $this->generateValidToken(['scopes' => ['read']]);
+        $this->assertFalse($authorization->Authorize($token, ['delete']));
+        $this->assertInstanceOf(\Exception::class, $authorization->GetError());
+        $this->assertStringContainsString('token does not have required scopes', $authorization->GetError()->getMessage());
+    }
+
+    /**
+     * @covers \Lin\AppPhp\Authorization\OAuthAuthorization::Authorize
+     * @covers \Lin\AppPhp\Authorization\OAuthAuthorization::IsTokenRevoked
+     * @covers \Lin\AppPhp\Authorization\OAuthAuthorization::GetError
+     */
+    public function testAuthorizeRevokedTokenThrowsException()
+    {
+        $authorization = $this->getMockForAbstractClass(OAuthAuthorization::class, [$this->publicKeyUri]);
+        $authorization->expects($this->once())
+            ->method('IsTokenRevoked')
+            ->with('abc123')
+            ->willReturn(true);
+
+        $token = $this->generateValidToken(['scopes' => ['read', 'write']]);
+        $this->assertFalse($authorization->Authorize($token, ['read', 'write']));
+        $this->assertInstanceOf(\Exception::class, $authorization->GetError());
+        $this->assertStringContainsString('token is revoked', $authorization->GetError()->getMessage());
+    }
+
+    /**
+     * @covers \Lin\AppPhp\Authorization\OAuthAuthorization::Authorize
+     * @covers \Lin\AppPhp\Authorization\OAuthAuthorization::IsTokenRevoked
+     * @covers \Lin\AppPhp\Authorization\OAuthAuthorization::GetError
+     */
+    public function testAuthorizeTokenWithoutScopes()
+    {
+        $authorization = new SampleAuth($this->publicKeyUri);
+
+        $token = $this->generateValidToken();
+        $this->assertTrue($authorization->Authorize($token));
+        $this->assertNull($authorization->GetError());
+    }
+
+    private function generateValidToken(array $payload = [])
+    {
+        $header = [
+            'alg' => 'RS256',
+            'typ' => 'JWT',
         ];
-        $PassToken = JWT::encode($Payload, $Key, 'RS256');
-        $Payload['jti'] = 'FAIL';
-        $FailToken = JWT::encode($Payload, $Key, 'RS256');
 
-        //create an instance of OAuthAuthorization
-        $Authorization = new class(__DIR__ . '/asset/public.key') extends OAuthAuthorization {
-            protected function IsTokenRevoked($JTI)
-            {
-                return $JTI == 'FAIL';
-            }
-        };
+        $payload = array_merge([
+            'iss' => 'https://example.com',
+            'sub' => '1234567890',
+            'iat' => time(),
+            'exp' => time() + 3600,
+            'jti' => 'abc123',
+        ], $payload);
 
-        //test if the instance is created
-        $this->assertInstanceOf(OAuthAuthorization::class, $Authorization);
+        $secret = file_get_contents(__DIR__ . '/asset/private.key');
 
-        //test if authorization works on valid token
-        $this->assertTrue($Authorization->Authorize($PassToken, ['doc:read']));
-        $this->assertNull($Authorization->GetError());
-
-        //test if authorization works on invalid token (out of scopes)
-        $this->assertFalse($Authorization->Authorize($PassToken, ['doc:delete']));
-        $this->assertEquals('token does not have required scopes', $Authorization->GetError()->getMessage());
-
-        //test if authorization works without giving scopes
-        $this->assertTrue($Authorization->Authorize($PassToken));
-        $this->assertNull($Authorization->GetError());
-
-        //test if authorization works on invalid token (revoked)
-        $this->assertFalse($Authorization->Authorize($FailToken));
-        $this->assertEquals('token is revoked', $Authorization->GetError()->getMessage());
+        return JWT::encode($payload, $secret, 'RS256', null, $header);
     }
 }
